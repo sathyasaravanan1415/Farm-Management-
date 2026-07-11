51b273ebf86a9b9682c616fd701969b531825f07
SATHYA SARAVANAN <sathya>
2026-07-11 23:21:41 +0530

    stage1

---
+++ FarmProject/Farmers/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Farmer(models.Model):
+    name=models.CharField(max_length=100)
+    phone=models.CharField(max_length=100)
+    email=models.EmailField()
+    address=models.TextField()
