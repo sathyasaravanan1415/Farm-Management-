9640612acf3ca890e2711111ec6e408627b5c634
sathyasaravanan1415 <saravana123er@gemail@gmail.com>
2026-08-18 14:38:55 +0530

    account api stage2

--- FarmProject/Accounts/urls.py
+++ FarmProject/Accounts/urls.py
@@ -7,3 +7,11 @@ router.register(r'profiles', FarmerProfileViewSet)
 router.register(r'register', RegisterViewSet, basename='register')
 
 urlpatterns = router.urls
+
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = router.urls + [
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
--- FarmProject/FarmProject/settings.py
+++ FarmProject/FarmProject/settings.py
@@ -133,3 +133,20 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
